07c96f051a53e29ebed05839f50e5dda2741a558
Ankit <ankit.gupta4@ing.com>
2023-02-19 00:51:24 +0100

    more

--- gcp/jupyter/deploy.sh
+++ gcp/jupyter/deploy.sh
@@ -1,2 +1,17 @@
-gcloud deployment-manager deployments create jupyterdeployment --config jupyter.yaml
+#/bin/bash
 
+### Checking arguments. 
+
+if [ $# -eq 0 ];
+then
+  echo "$0: Missing arguments"
+  exit 1
+elif [ $# -gt 1 ];
+then
+  echo "$0: Too many arguments: $@"
+  exit 1
+vmname=$0
+mydate=$(date)
+deploymentname="${vmname}_${mydate}_deployment"
+echo $vmname
+gcloud deployment-manager deployments create $deploymentname --config jupyter.yaml
