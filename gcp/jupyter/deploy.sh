23570d6b7d97790f2b0c123a298cb1f27a245935
Ankit <ankit.gupta4@ing.com>
2023-02-19 01:14:47 +0100

    more

--- gcp/jupyter/deploy.sh
+++ gcp/jupyter/deploy.sh
@@ -10,8 +10,12 @@ elif [ $# -gt 1 ];
 then
   echo "$0: Too many arguments: $@"
   exit 1
-vmname=$0
-mydate=$(date)
-deploymentname="${vmname}_${mydate}_deployment"
-echo $vmname
+fi
+vmname=$1;
+export vmname
+echo $vmname;
+mydate=$(date +%Y%m%d);
+echo $mydate;
+deploymentname="${vmname}${mydate}deployment";
+echo $vmname;
 gcloud deployment-manager deployments create $deploymentname --config jupyter.yaml
