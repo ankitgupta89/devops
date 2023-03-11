75bb295839324fc01ccb9448ffd79222c5063d75
ankit <ankit_gupta89@outlook.com>
2023-03-11 01:33:43 +0100

    asdf

--- gcp/ansible/deploy.sh
+++ gcp/ansible/deploy.sh
@@ -1,5 +1,4 @@
 sudo apt-get update && sudo apt-get install ansible -y
-git clone https://github.com/ankitgupta89/devops.git
 service_account_email=`gcloud iam service-accounts list | grep EMAIL | grep cli-service | awk '{ print $2 }'`
 gcloud iam service-accounts keys create devops/gcp/ansible/creds.json --iam-account $service_account_email
 ansible-playbook devops/gcp/ansible/rhel_vm.yaml
